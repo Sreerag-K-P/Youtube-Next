4451da3d34c3ac9dfcad41672e1b6813c42a49de
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-11-09 11:38:05 +0530

    make it dynamic by revalidating

--- app/[selected]/page.jsx
+++ app/[selected]/page.jsx
@@ -1,7 +1,7 @@
 import ClientSide from "@/components/ClientSide";
 import React from "react";
 import getDataItems from "../libs/getDataItems";
-
+export const revalidate = 0; // revalidate at most every time
 async function page({ params }) {
   const datas = await getDataItems(`search?part=snippet&q=${params.selected}`);
   return (
